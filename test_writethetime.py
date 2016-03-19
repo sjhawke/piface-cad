f53d947516f3df3299148d6bc1d0c718c0496848
Simon Hawke <simonjhawke@gmail.com>
2016-03-19 23:44:05 +0000

    Added test to demonstrate that some times may be too long to display perfectly on the 16x2 LCD

--- test_writethetime.py
+++ test_writethetime.py
@@ -14,67 +14,56 @@ class TestGetTimeAsWords(unittest.TestCase):
     def testMidnight(self):
         dt = datetime.datetime(2016,2,28,0,0,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Midnight",
-                         "expect midnight")
+                         "Midnight")
     def testOneAm(self):
         dt = datetime.datetime(2016,2,28,1,0,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "One O'Clock AM",
-                         "expect 1am")
+                         "One O'Clock AM")
 
     def testTwoAm(self):
         dt = datetime.datetime(2016,2,28,2,0,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Two O'Clock AM",
-                         "expect 2am")
+                         "Two O'Clock AM")
 
     def testFivePastTwoAm(self):
         dt = datetime.datetime(2016,2,28,2,5,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Five Mins Past Two AM",
-                         "expect 2:05am")
+                         "Five Mins Past Two AM")
 
     def testQuarterPastTwoAm(self):
         dt = datetime.datetime(2016,2,28,2,15,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Quarter Past Two AM",
-                         "expect 2:15am")
+                         "Quarter Past Two AM")
                          
     def testMidday(self):
         dt = datetime.datetime(2016,2,28,12,0,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Midday",
-                         "expect 12pm")
+                         "Midday")
 
     def testHalfPastTwoPm(self):
         dt = datetime.datetime(2016,2,28,14,30,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Half Past Two PM",
-                         "expect 2:30pm")
+                         "Half Past Two PM")
 
     def testHalfPastMidday(self):
         dt = datetime.datetime(2016,2,28,12,30,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Half Past Midday",
-                         "expect 12:30pm")
+                         "Half Past Midday")
                          
     def testTwelveFortyFivePM(self):
         dt = datetime.datetime(2016,2,28,12,45,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Quarter To One PM",
                          "Quarter To One PM")
     
     def testElevenFortyFivePM(self):
         dt = datetime.datetime(2016,2,28,23,45,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Quarter To Midnight",
-                         "Quarter To 12 Midnight")
+                         "Quarter To Midnight")
                     
     def testElevenFortyFiveAM(self):
         dt = datetime.datetime(2016,2,28,11,45,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
-                         "Quarter To Midday",
-                         "Quarter To 12 AM")
+                         "Quarter To Midday")
 
     def testTenFourteenPM(self):
         dt = datetime.datetime(2016,2,28,22,14,0)
@@ -101,6 +90,11 @@ class TestGetTimeAsWords(unittest.TestCase):
         dt = datetime.datetime(2016,2,28,0,1,0)
         self.assertEqual(writethetime.getTimeAsWords(dt), 
                          "One Min Past Midnight")
+    
+    def testVeryLongString(self):
+        dt = datetime.datetime(2016,2,28,11,27,0)
+        self.assertEqual(writethetime.getTimeAsWords(dt), 
+                         "Twenty-seven Mins Past Eleven AM")
 
 class TestWrap16x2(unittest.TestCase):
     def testTextWrappingTwoLines(self):
@@ -115,3 +109,11 @@ class TestWrap16x2(unittest.TestCase):
         result = writethetime.wrap16x2(text)
         expected = "short string"
         self.assertEqual(expected, result)
+        
+    def testTextWrappingTwoLinesOverLength(self):
+        text = "Twenty-seven Mins Past Eleven AM"
+        result = writethetime.wrap16x2(text)
+        #           1234567890123456
+        expected = "Twenty-seven\nMins Past Eleven"
+        self.assertEqual(expected, result)
+        
